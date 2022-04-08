943e4ac086d2c3137da5f007e0cabb07a93524ba
funam <funamario071@gmail.com>
2022-04-08 23:58:40 +0900

    [3の倍数]を学習しました。

--- 02_レシピ/01_□と○/main.js
+++ 02_レシピ/01_□と○/main.js
@@ -1,10 +1,1 @@
-const genkiFunction = function (number) {
-  for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
-      console.log(n + "!!!!!")
-    } else {
-      console.log(n)
-    }
-  }
-}
-genkiFunction(100)
+
