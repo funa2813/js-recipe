52562b5099df4abc01034f5bde33c5536b970756
funam <funamario071@gmail.com>
2022-04-10 00:10:19 +0900

    [□と○]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,7 +1,17 @@
 const display = document.getElementById("display")
-const plusButton = document.getElementById("plus-button")
+const plusButton0 = document.getElementById("plus-button0")
+const plusButton1 = document.getElementById("plus-button1")
+const plusButton2 = document.getElementById("plus-button2")
 let count = 0
-plusButton.onclick = function () {
+plusButton0.onclick = function () {
   count += 1
+  display.textContent = count
+}
+plusButton1.onclick = function () {
+  count -= 1
+  display.textContent = count
+}
+plusButton2.onclick = function () {
+  count *= 2
   display.textContent = count
 }
